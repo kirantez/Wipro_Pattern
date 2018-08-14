ee1bc24f1efa6b644f8f9527e0411773e0dab75e
Kiran_Tej <34706650+kirantez@users.noreply.github.com>
2018-08-14 17:53:39 +0530

    Add files via upload

---
+++ wipropattern.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,num,temp,var,k,l,x=1,y,count=0;
+    printf("Enter the number: \n");
+    scanf("%d",&num);
+    var=num*(num+1);
+    var=var-(num-1);
+	temp=var;
+    for (i=num; i>0; i--)
+    {
+	if (count)
+	    for (k=0;k<count;k++)
+	    printf("--");
+	for (j=1; j<=i; j++)
+	{
+	    
+	    printf("%d",x);
+	    printf("*");
+	    x++;
+	    
+	}
+	for (j=1; j<=i; j++)
+	{
+	    printf("%d",temp++);
+	    printf("*");
+	}
+	    printf("\n");
+	    count++;
+    var=var-(i-1);
+	temp=var;
+
+    }
+}
